8dae4edd1b6accc9e91b833d2cd32f4ace4dc3ab
trong <trongr@users.noreply.github.com>
2017-04-06 02:09:15 -0400

    Preprocess data

--- titanic.py
+++ titanic.py
@@ -13,3 +13,14 @@ titanic.download_dataset('titanic_dataset.csv')
 data, labels = load_csv('titanic_dataset.csv', target_column=0,
                         categorical_labels=True, n_classes=2)
 
+def preprocess(data, columns_to_ignore):
+    for id in sorted(columns_to_ignore, reverse=True):
+        for i in range(len(data)):
+            data[i].pop(id)
+    for i in range(len(data)):
+        # Converting 'sex' field to float (id is 1 after removing labels column)
+        data[i][1] = 1 if data[i][1] == 'female' else 0 
+    return np.array(data, dtype=np.float32)
+
+columns_to_ignore = [1, 6] # Ignore 'name' and 'ticket' columns (id 1 & 6 of data array)
+data = preprocess(data, columns_to_ignore)
