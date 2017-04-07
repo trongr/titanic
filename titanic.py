8c25a6939547ffcc3aa38eb5f74bc217e91a4fc1
trong <trongr@users.noreply.github.com>
2017-04-06 23:00:35 -0400

    Build model, training and testing

--- titanic.py
+++ titanic.py
@@ -24,3 +24,23 @@ def preprocess(data, columns_to_ignore):
 
 columns_to_ignore = [1, 6] # Ignore 'name' and 'ticket' columns (id 1 & 6 of data array)
 data = preprocess(data, columns_to_ignore)
+
+net = tflearn.input_data(shape=[None, 6])
+net = tflearn.fully_connected(net, 32)
+net = tflearn.fully_connected(net, 32)
+net = tflearn.fully_connected(net, 2, activation='softmax')
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+# Start training (apply gradient descent algorithm)
+model.fit(data, labels, n_epoch=10, batch_size=16, show_metric=True)
+
+# Testing
+dicaprio = [3, 'Jack Dawson', 'male', 19, 0, 0, 'N/A', 5.0000]
+winslet = [1, 'Rose DeWitt Bukater', 'female', 17, 1, 2, 'N/A', 100.0000]
+
+dicaprio, winslet = preprocess([dicaprio, winslet], columns_to_ignore)
+
+pred = model.predict([dicaprio, winslet])
+print("DiCaprio Surviving Rate:", pred[0][1])
+print("Winslet Surviving Rate:", pred[1][1])
